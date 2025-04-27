562351a73e8f27743cc5e13a33cc361a65fe44c7
yunzhen06 <11336011@ntub.edu.tw>
2025-04-27 16:02:38 +0800

    更新連結

--- student_platform_frontend/src/App.js
+++ student_platform_frontend/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/Login';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import OAuthCallback from './components/OAuthCallback';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        {/* 這裡可以添加更多路由 */}
-        <Route path="*" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        {/*
+          建議對 Dashboard 路由加入授權保護（例如：私有路由），
+          以避免未授權存取，這裡僅做提示，實作時可加入驗證機制。
+        */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/oauth-callback" element={<OAuthCallback />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
