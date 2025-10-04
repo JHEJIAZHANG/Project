7463997c8e1e9cdf633e346d75c5e59318014a31
JHEJIAZHANG <studyfromzhang0809@gmail.com>
2025-10-05 04:44:45 +0800

    temp: remove classroomai

--- classroomai/classroomai/__init__.py
+++
@@ -1,4 +0,0 @@
-# 這將確保 Django 啟動時載入 Celery 應用
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
--- classroomai/classroomai/admin.py
+++
@@ -1,4 +0,0 @@
-# classroomai/admin.py
-from django.contrib import admin
-
-# 使用Django默認的admin site，移除所有自定義功能
